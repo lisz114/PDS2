537e1c8c640ac4b7698c6c27632a1f18de3018ab
Elisa <vitoriaelisa114@gmail.com>
2024-12-04 08:34:07 -0300

    AssistecMVC atualizado

--- AssistecMVC/src/main/java/br/com/loja/assistec/controller/ListarUsuarioController.java
+++ AssistecMVC/src/main/java/br/com/loja/assistec/controller/ListarUsuarioController.java
@@ -2,18 +2,25 @@ package br.com.loja.assistec.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import br.com.loja.assistec.model.Usuario;
+import br.com.loja.assistec.model.UsuarioDAO;
 import br.com.loja.assistec.view.ListarUsuariosView;
+import br.com.loja.assistec.view.MensagemView;
 
 public class ListarUsuarioController {
-
 	private ListarUsuariosView listarView;
 	
-	public ListarUsuarioController() {
+	public ListarUsuarioController() throws SQLException {
 		listarView = new ListarUsuariosView();
 		listarView.setLocationRelativeTo(null);
-		listaView.setVisible(true);
+		listarView.setVisible(true);
 		configurarListeners();
 		carregarUsuarios();
 	}
@@ -21,21 +28,43 @@ public class ListarUsuarioController {
 	public void configurarListeners() {
 		listarView.addListarUsuariosListener(new ListarUsuariosListener());
 		listarView.addWindowListener(new JanelaAberturaListener());
+		listarView.addTabelaMouseListener(new TabelaMouseClickListener());
+	}
+
+	public void carregarUsuarios() throws SQLException {
+		try {
+		ArrayList<Usuario> listaUsuarios = listarUsuarios();
+		if(!listaUsuarios.isEmpty()) {
+			listarView.mostrarUsuariosTabela(listaUsuarios);
+		}
+	} catch (SQLException e) {
+		new MensagemView("Erro ao carregar usuários!",0);
+	}
 	}
 	
-	public void carregarUsuarios() {
+	public ArrayList<Usuario> listarUsuarios() throws SQLException{
+		UsuarioDAO dao = new UsuarioDAO();
+		return dao.selecionarUsuarios();
+	}
+	
+	//Classe que trata o evento do clique na tabela
+	private class TabelaMouseClickListener extends MouseAdapter{
 		
+		public void mouseClicked(MouseEvent e) {
+			if (e.getButton() == MouseEvent.BUTTON1) {
+				//Selecionar o usuario
+				System.out.println("clicado no botao esquerdo do mouse");
+			}
+		}
 	}
 	
-}
-
+	//Classe que trata os eventos de botao
 	private class ListarUsuariosListener implements ActionListener {
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String comando = e.getActionCommand();
-			switch(comando) {
-			case "BotaoCadastrarAction":
+			switch (comando) {
+			case "BotaoCadastrarAction": 
 				abrirCadastroUsuario(null);
 				break;
 			case "BotaoFecharAction":
@@ -46,8 +75,19 @@ public class ListarUsuarioController {
 			}
 		}
 		
-		public void abrirCadastroUsuario(Usuario usuarioSelecionado) {
-			
-		}
-		
 	}
+	
+	public void abrirCadastroUsuario(Usuario usuarioSelecionado) {
+		new CadastrarUsuarioController(this, usuarioSelecionado);
+	}
+	
+	private class JanelaAberturaListener extends WindowAdapter{
+		public void windowOpened(WindowEvent e) {
+			try {
+				carregarUsuarios();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+}
--- AssistecMVC/src/main/java/br/com/loja/assistec/view/MensagemView.java
+++ AssistecMVC/src/main/java/br/com/loja/assistec/view/MensagemView.java
@@ -1,73 +1,144 @@
-listarusuariosview
 package br.com.loja.assistec.view;
 
-import java.awt.EventQueue;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.table.TableRowSorter;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
-import br.com.loja.assistec.controller.UsuarioController;
-import br.com.loja.assistec.model.Usuario;
-import br.com.loja.assistec.model.UsuarioTableModel;
-
-public class ListarUsuariosView extends JFrame {
+public class MensagemView extends JDialog {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField txtLocalizar;
-	private JButton btSim;
-	private JButton btNao;
-	private JButton btnCadastrar;
-	private JButton btnFechar;
-	private JTable tabela;
-	private JScrollPane scrollPane;
-	private ArrayList<Usuario> usuariosList;
-	private ListarUsuariosView listarUsusariosView;
-	private UsuarioTableModel usuarioTableModel;
-	private TableRowSorter<UsuarioTableModel> rowSorter;
+	private int resposta;
 
-	public ListarUsuariosView() {
-		inicializarComponentes();
-		configurarJanela();
+	public MensagemView(String mensagem, int tipo) {
+		setTitle("Mensagem");
+		setModal(true);
+
+		// Criar Panel
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		add(panel);
+
+		// Rótulo para mensagem
+		JLabel lblMensagem = new JLabel(mensagem, SwingConstants.CENTER);
+		panel.add(lblMensagem, BorderLayout.CENTER);
+
+		// caminho do icone
+		String iconPath;
+		switch (tipo) {
+		case 0: // Erro
+			iconPath = "/br/com/loja/assistec/icones/erro.png";
+			lblMensagem.setIcon(new ImageIcon(getClass().getResource(iconPath)));
+			break;
+		case 1: // Informação
+			iconPath = "/br/com/loja/assistec/icones/info.png";
+			lblMensagem.setIcon(new ImageIcon(getClass().getResource(iconPath)));
+			break;
+		case 2: // Atenção
+			iconPath = "/br/com/loja/assistec/icones/alerta.png";
+			lblMensagem.setIcon(new ImageIcon(getClass().getResource(iconPath)));
+			break;
+		case 3: // Sucesso
+			iconPath = "/br/com/loja/assistec/icones/sucesso.png";
+			lblMensagem.setIcon(new ImageIcon(getClass().getResource(iconPath)));
+			break;
+		case 10: // Assistec
+			iconPath = "/br/com/loja/assistec/icones/assistec.png";
+			lblMensagem.setIcon(new ImageIcon(getClass().getResource(iconPath)));
+			break;
+		}
+
+		JPanel painelSul = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+		// Cria um esaço vazio
+		painelSul.add(new JPanel());
+		JButton btnOK = new JButton("OK");
+		// Tamanho do botao
+		btnOK.setPreferredSize(new Dimension(100, 30));
+		btnOK.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		painelSul.add(btnOK);
+		painelSul.add(new JPanel());
+
+		painelSul.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		panel.add(painelSul, BorderLayout.SOUTH);
+
+		getRootPane().setDefaultButton(btnOK);
+		setSize(350, 200);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+
 	}
 
+	// Método para mensagens de perguntas
+	public MensagemView(String pergunta) {
+		setTitle("Mensagem");
+		setModal(true);
 
-	public void inicializarComponentes() {
-		btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.setActionCommand("BotaoCadastrarAction");
-		btnCadastrar.setBounds(10, 11, 117, 23);
-		
-		txtLocalizar = new JTextField();
-		txtLocalizar.setBounds(139, 12, 252, 20);
-		
-		btnFechar = new JButton("Cadastrar");
-		btnFechar.setActionCommand("BotaoFecharAction");
-		btnFechar.setBounds(269, 337, 89, 33);
-		
-		tabela = new JTable();
-		scrollPane = new JScrollPane(tabela);
-		scrollPane.setBounds(39, 95, 530, 215);
-		
-		setLayout(null);
-		add(btnCadastrar);
-		add(btnFechar);
-		add(txtLocalizar);
-		add(scrollPane);
+		// Criar Panel
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		add(panel);
+
+		JLabel labelPergunta = new JLabel(pergunta, SwingConstants.CENTER);
+		String iconPath;
+		iconPath = "/br/com/loja/assistec/icones/question.png";
+		labelPergunta.setIcon(new ImageIcon(getClass().getResource(iconPath)));
+		panel.add(labelPergunta, BorderLayout.CENTER);
+
+		JPanel painelSul = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+		JButton btnSim = new JButton("Sim");
+		// Tamanho do botao
+		btnSim.setPreferredSize(new Dimension(100, 30));
+		btnSim.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resposta = 1;
+				setVisible(false);
+			}
+		});
+
+		painelSul.add(btnSim);
+
+		JButton btnNao = new JButton("Não");
+		// Tamanho do botao
+		btnNao.setPreferredSize(new Dimension(100, 30));
+		btnNao.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resposta = 0;
+				setVisible(false);
+			}
+		});
+		painelSul.add(btnNao);
+
+		painelSul.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		panel.add(painelSul, BorderLayout.SOUTH);
+
+		setSize(400, 200);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+
 	}
-	
-	public void configurarJanela() {
-		setTitle("Listagem de usuários");
-		setBounds(100, 100, 450, 300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	public int getResposta() {
+		return resposta;
 	}
-	
-
 }
